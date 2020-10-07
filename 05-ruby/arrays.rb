c78168d8b69ab62d2cb038b47aa661d9be56e944
JT <certainthings@gmail.com>
2020-10-07 15:20:24 +1100

    array and hash exercise solutions

---
+++ 05-ruby/arrays.rb
@@ -0,0 +1,47 @@
+require 'pry'
+
+# ### 1. Create an array of the days of the week
+# - Create a variable named `days_of_the_week` as an array of the following:
+#     - Monday
+#     - Tuesday
+#     - Wednesday
+#     - Thursday
+#     - Friday
+#     - Saturday
+#     - Sunday
+
+days_of_the_week = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+
+# p - programmer print: it preserves information about our data and structures:
+p days_of_the_week
+
+# ### 2. My calendar says the first day is Sunday...
+# - Remove Sunday from the last postion and move it to the first position.  Use array methods.
+
+# Long winded but explicit
+# moving_day = days_of_the_week.pop
+# days_of_the_week.unshift moving_day
+
+# Very clever
+# days_of_the_week.unshift days_of_the_week.pop
+
+# Most Rubyish
+days_of_the_week.rotate! -1
+p days_of_the_week
+
+#
+# ### 3. Create a new array of the days of the week:
+# - The first inner array should be the weekdays
+# - The second inner array should be the weekend days
+
+day_parts = [ days_of_the_week[1..5], days_of_the_week.rotate.drop(5) ]
+p day_parts
+
+#
+# ### 4. Remove either the weekdays or the weekends
+# Your choice...
+day_parts.pop
+p day_parts
+
+# ### 5. Sort the remaining days alphabetically
+p day_parts.flatten.sort
