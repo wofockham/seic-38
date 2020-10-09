29181ba7bad027fe048121f18fdde7c3e006523c
JT <certainthings@gmail.com>
2020-10-09 12:57:26 +1100

    nav

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -8,17 +8,22 @@ end
 
 # INDEX
 get '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  @butterflies = db.execute 'SELECT * FROM butterflies'
+  @butterflies = query_db 'SELECT * FROM butterflies'
   erb :butterflies_index
 end
 
 # SHOW
 get '/butterflies/:id' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
   @butterfly = @butterfly.first # Extract the butterfly from the array.
   erb :butterflies_show
+end
+
+def query_db(sql_statement)
+  puts sql_statement # Optional but nice for debugging
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  results = db.execute sql_statement
+  db.close
+  results # implicitly returned
 end
