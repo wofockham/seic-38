a82827342e734172ec41b51a96be07ad978b919e
JT <certainthings@gmail.com>
2020-10-09 12:40:53 +1100

    index and show

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -5,3 +5,20 @@ require 'sqlite3'
 get '/' do
   erb :home
 end
+
+# INDEX
+get '/butterflies' do
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  @butterflies = db.execute 'SELECT * FROM butterflies'
+  erb :butterflies_index
+end
+
+# SHOW
+get '/butterflies/:id' do
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  @butterfly = db.execute "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  @butterfly = @butterfly.first # Extract the butterfly from the array.
+  erb :butterflies_show
+end
