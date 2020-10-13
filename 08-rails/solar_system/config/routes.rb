fc088b98a1bcccfb46d47e04d45f279af01241b7
JT <certainthings@gmail.com>
2020-10-13 14:39:50 +1100

    planets index and show

--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/planets' => 'planets#index'
+  get '/planets/:id' => 'planets#show', :as => 'planet'
 end
