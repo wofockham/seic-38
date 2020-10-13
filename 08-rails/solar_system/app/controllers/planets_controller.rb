efe9035af2956880b6ec9fe52157d82d321ff644
JT <certainthings@gmail.com>
2020-10-13 16:42:49 +1100

    planets CRUD

--- 08-rails/solar_system/app/controllers/planets_controller.rb
+++ 08-rails/solar_system/app/controllers/planets_controller.rb
@@ -14,4 +14,26 @@ class PlanetsController < ApplicationController
     planet = Planet.create :name => params[:name], :image => params[:image], :orbit => params[:orbit], :diameter => params[:diameter], :mass => params[:mass], :moons => params[:moons]
     redirect_to planet_path(planet.id) # show
   end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.name = params[:name]
+    planet.image = params[:image]
+    planet.orbit = params[:orbit]
+    planet.diameter = params[:diameter]
+    planet.mass = params[:mass]
+    planet.moons = params[:moons]
+    planet.save
+    redirect_to planet_path(planet.id) # show
+  end
+
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy # Alderaan
+    redirect_to planets_path # index
+  end
 end
--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -1,7 +1,10 @@
 # /planets/new
 Rails.application.routes.draw do
   get '/planets' => 'planets#index'
-  get '/planets/new' => 'planets#new'
+  get '/planets/new' => 'planets#new', :as => 'new_planet'
   post '/planets' => 'planets#create'
   get '/planets/:id' => 'planets#show', :as => 'planet'
+  get '/planets/:id/edit' => 'planets#edit', :as => 'edit_planet'
+  post '/planets/:id' => 'planets#update'
+  delete '/planets/:id' => 'planets#destroy'
 end
