486e984193310d5f2a6e90e5ad89849c0087bd9d
JT <certainthings@gmail.com>
2020-10-14 16:41:11 +1100

    more view helper examples

--- 08-rails/view-helpers/config/routes.rb
+++ 08-rails/view-helpers/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'pages#home' # root_path
-  get '/numbers' => 'pages#numbers'
+  get '/mcnumbers' => 'pages#numbers', :as => 'numbers'
   get '/text' => 'pages#text'
   get '/assets' => 'pages#assets'
   get '/url' => 'pages#url'
