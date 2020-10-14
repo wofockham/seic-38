9f9fe3707c6a418f4d5ebabf6c6aa21e5df9668d
JT <certainthings@gmail.com>
2020-10-14 15:16:24 +1100

    artist CRUD is done

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -10,4 +10,32 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new # blank artist template
   end
+
+  def create
+    artist = Artist.create artist_params
+    redirect_to artist
+  end
+
+  def edit
+    @artist = Artist.find params[:id] # existing artist
+  end
+
+  def update
+    artist = Artist.find params[:id]
+    artist.update artist_params
+    redirect_to artist
+  end
+
+  def destroy
+    artist = Artist.find params[:id]
+    artist.destroy
+    redirect_to artists_path # index
+  end
+
+  private
+  # Strong parameters
+  def artist_params
+    # White list of parameters that we are happy to receive from the browser.
+    params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
+  end
 end
