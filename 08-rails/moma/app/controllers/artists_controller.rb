ac798d2db881a4887afbc7d12cbc3b2513120b66
JT <certainthings@gmail.com>
2020-10-14 11:45:58 +1100

    artist#new

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -2,4 +2,12 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find params[:id]
+  end
+
+  def new
+    @artist = Artist.new # blank artist template
+  end
 end
