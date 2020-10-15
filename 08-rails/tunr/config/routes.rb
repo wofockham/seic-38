cafcee52e57b9990cac9d650228adba44c17d365
JT <certainthings@gmail.com>
2020-10-15 16:23:56 +1100

    user sign up

--- 08-rails/tunr/config/routes.rb
+++ 08-rails/tunr/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root :to => 'pages#home'
   resources :users, :only => [:new, :create]
 end
--- 08-rails/tunr/db/schema.rb
+++ 08-rails/tunr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_013628) do
+ActiveRecord::Schema.define(version: 2020_10_15_041250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_013628) do
     t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
