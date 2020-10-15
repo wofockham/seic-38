0a1f5e90ec9d33e9a2cd0edc0306a61d6ed52c2b
JT <certainthings@gmail.com>
2020-10-15 14:13:56 +1100

    seed data with associations

--- 08-rails/tunr/db/seeds.rb
+++ 08-rails/tunr/db/seeds.rb
@@ -39,3 +39,32 @@ m2 = Mixtape.create :title => 'Makeout Music'
 m3 = Mixtape.create :title => 'Code Jams'
 m4 = Mixtape.create :title => 'Monkey Mongs'
 puts "#{ Mixtape.count } mixtapes."
+
+# Associations
+puts "Albums and songs."
+l1.songs << s1
+l2.songs << s2
+l3.songs << s3
+l4.songs << s4
+
+puts "Artists and songs."
+a1.songs << s1
+a2.songs << s2
+a3.songs << s3
+a4.songs << s4
+
+puts "Genres and songs."
+s1.genres << g1 << g2
+s2.genres << g5 << g6
+s3.genres << g3 << g4
+g6.songs << s4 # equivalent to: s4.genres << g6
+
+puts "Mixtapes and songs."
+m1.songs << s1 << s2 << s3 << s4
+m2.songs << s2 << s2 << s2 << s2
+m3.songs << s2 << s4 << s2 << s4
+m4.songs << s4 << s3 << s2 << s2
+
+puts "Mixtapes and users"
+u1.mixtapes << m1 << m2
+u2.mixtapes << m3 << m4 << m2
