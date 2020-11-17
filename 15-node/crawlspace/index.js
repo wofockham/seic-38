c1c64ab6dcd6c60bd6852293b35c6da2476f0957
JT <certainthings@gmail.com>
2020-11-17 12:41:14 +1100

    web crawler

---
+++ 15-node/crawlspace/index.js
@@ -0,0 +1,49 @@
+const Crawler = require('crawler');
+
+// Predicate function to decide if we want to add a URL to the queue.
+const isCrawlable = (url) => {
+  if (! url) return false;
+  if (! url.startsWith('http')) return false;
+
+  const terribleSites = ['archive.md', 'facebook.com', 'medium.com', 'twitter.com'];
+  return ! terribleSites.some( awfulSite => url.includes(awfulSite) ); // Heavy magic
+
+  // const awfulSiteDetected = terribleSites.some((awfulSite) => {
+  //   if (url.includes(awfulSite)) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // })
+
+  // if (awfulSiteDetected === true) {
+  //   return false;
+  // }
+  //
+  // return true; // default
+};
+
+const c = new Crawler({
+  rateLimit: 1000,
+  callback: function (err, res, done) { // error first pattern
+    if (typeof res.$ === 'function') {
+      const title = res.$('title').text();
+      console.log( title ); // Show the title
+      console.log( res.request.uri.href ); // Show you the URL
+      console.log(); // Create some space
+
+      const $links = res.$('a');
+
+      $links.each(function () {
+        const href = res.$(this).attr('href');
+        if (isCrawlable(href)) {
+          c.queue( href );
+        }
+      });
+    }
+
+    done();
+  }
+});
+
+c.queue('https://lobste.rs/'); // TODO: let the user provide this via the command line
