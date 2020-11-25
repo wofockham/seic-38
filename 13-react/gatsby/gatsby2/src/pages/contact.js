968237fcbc8f24a478dd2ac3f175fe88104a2273
JT <certainthings@gmail.com>
2020-11-25 11:30:27 +1100

    more gatsby

---
+++ 13-react/gatsby/gatsby2/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default function Contact () {
+  return (
+    <Layout>
+      <h1>Contact Us</h1>
+      <p>Please do not contact us</p>
+    </Layout>
+  )
+}
