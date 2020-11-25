b8ef650f364bfffea8958209c63e63c4c51033d5
JT <certainthings@gmail.com>
2020-11-26 10:58:38 +1100

    gatsby with emotion -- thanks ryan

---
+++ 13-react/gatsby/gatsby3/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About Inspired By Kittens</h1>
+      <p>
+        lorem ipsum coming soon
+      </p>
+    </Layout>
+  )
+}
---
+++ 13-react/gatsby/gatsby3/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from '../components/layout';
+
+export default function Home () {
+  return (
+    <Layout>
+      <h1>Inspired By Kittens</h1>
+      <div>
+        <img src="http://www.placekitten.com/800/1000" />
+      </div>
+    </Layout>
+  )
+}
